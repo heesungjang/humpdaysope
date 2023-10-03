63bd762047f12151618c5a116d9f9bd4ee967374
heesungjang <heesungj7@gmail.com>
2023-10-04 01:00:33 +1000

    dark as default

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,17 @@ export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center  p-24 justify-center lg:justify-start">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm  lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-neutral-700 bg-gradient-to-b  pb-6 pt-8 backdrop-blur-2xl  bg-zinc-800/30 from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:bg-zinc-800/30">
           Happy Hump Day &nbsp;
           <code className="font-mono font-bold">Sope Teams 🎉</code>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none sm:hidden lg:block transition-all">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t   from-black via-black lg:static lg:h-auto lg:w-auto lg:bg-none sm:hidden lg:block transition-all">
           <a className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
             <Image
               src="/sopelogo.png"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
+              alt="Sope Logo"
+              className="invert"
+              width={120}
               height={24}
               priority
             />
@@ -28,7 +28,7 @@ export default function Home() {
           href="https://wheelofnames.com/"
           target="_blank"
           rel="noopener noreferrer"
-          className={`text-xl font-semibold mt-14 group rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30`}
+          className={`text-xl font-semibold mt-14 group rounded-lg border  px-5 py-4 transition-colors bg-gray-100 border-neutral-700 bg-neutral-800/30`}
         >
           Spin the Wheel 🎡
         </Link>
@@ -40,7 +40,7 @@ export default function Home() {
             href="https://www.theage.com.au/topic/quizzes-1qx"
             target="_blank"
             rel="noopener noreferrer"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors  hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
               The Age{' '}
@@ -57,7 +57,7 @@ export default function Home() {
             href="https://www.nytimes.com/games/wordle/index.html"
             target="_blank"
             rel="noopener noreferrer"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors  hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
               Wordle{' '}
@@ -72,7 +72,7 @@ export default function Home() {
 
           <a
             href="https://worldle.teuteuf.fr/"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors  hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -94,7 +94,7 @@ export default function Home() {
 
           <a
             href="https://www.geoguessr.com/"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors  hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -112,7 +112,7 @@ export default function Home() {
 
           <a
             href="https://emovi.teuteuf.fr/"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors  hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -129,7 +129,7 @@ export default function Home() {
 
           <a
             href="https://framed.wtf/"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors  hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
           >
