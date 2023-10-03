3e122b004c9a8d47913b38359e4f1695b74291b3
heesungjang <heesungj7@gmail.com>
2023-10-04 00:27:15 +1000

    meta data update

---
+++ app/wordle/page.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className="flex h-screen flex-col items-center  p-24 justify-center lg:justify-start"></main>
+  );
+}
